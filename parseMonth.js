ce6c74c3a8bb2f955bded5c9d4d081b46fd666a7
Oliver Boudet <oboudet1@me.com>
2021-01-23 23:07:23 +0100

    add all months

--- parseMonth.js
+++ parseMonth.js
@@ -5,7 +5,13 @@ function parseMonth(month) {
     mar: "03",
     apr: "04",
     maj: "05",
-    jun: "06"
+    jun: "06",
+    jul: "07",
+    aug: "08",
+    sep: "09",
+    oct: "10",
+    nov: "11",
+    dec: "12"
   };
   return dateDict[month];
 }
